bfc7309335790238a14405340bf3ec4e9ac721fc
Ben Schembri <benschembri@gmail.com>
2022-09-05 17:51:04 +1000

    fleshed out the costumes controller a bit

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -1,22 +1,42 @@
 class CostumesController < ApplicationController
   def index
+    @costumes = Costume.all
   end
 
   def show
+    @costume = Costume.find(params[:id])
   end
 
   def new
+    @costume = Costume.new
   end
 
   def create
+    @costume = Costume.new(costume_params)
+    if @costume.save
+      redirect_to costume_path(@costume)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+
   end
 
   def update
+
   end
 
   def destroy
+    @costume = Costume.find(params[:id])
+    @costume.destroy
+    redirect_to costume_path, status: see_other
+  end
+
+  private
+
+  def costume_params
+    params.require(:costume).permit(:name, :description, :price, :type)
   end
 end
