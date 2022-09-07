b535ddd30dddc4d4f66f21a084af2b776c5aebf1
Ben Schembri <benschembri@gmail.com>
2022-09-07 12:49:39 +1000

    built out bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,12 +1,25 @@
 class BookingsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :new]
-
+  before_action :set_costume, only: %i[new create]
   def new
+    # Don't forget to add @costume in simple form
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.costume = @booking
+    @booking.save
+    redirect_to costume_path(@costume)
   end
 
-  def update
+  private
+
+  def set_costume
+    @costume = Costume.find(params[:costume_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
