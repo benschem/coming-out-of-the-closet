667783f98acb3c2338a19eb501b903248596e001
Micah <lavithian@hotmail.com>
2022-09-07 13:16:43 +0930

    cloudinary should be done. Added Seed pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,18 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 require "faker"
+require "open-uri"
 
 puts "Cleaning database..."
 User.destroy_all # if Rails.en.development
 Costume.destroy_all # if Rails.en.development
 clothing = ["suit", "costume", "cosplay"]
+pic1 = "./app/assets/images/pirate.jpg"
+pic2 = "./app/assets/images/IU.png"
+pic3 = "./app/assets/images/jinx.jpg"
+pic4 = "./app/assets/images/pj.png"
+pic5 = "./app/assets/images/suit.jpg"
+photos = [pic1, pic2, pic3, pic4, pic5]
 
 User.create!(
   username: "Music Mates",
@@ -36,6 +43,9 @@ puts "Created User: Music Mates, Email: music@mates.com, Password: 123456."
       clothing: clothing.sample,
       user: user
     )
+    # file = URI.open(photos[rand(0..4)])
+    costume.photo.attach(io: File.open(photos[rand(0..4)]), filename: "example", content_type: "image/png")
+    costume.save
     puts "Created Costume: #{costume.name}, a #{costume.clothing} for #{costume.user.username} #{costume.price}."
   end
 end
