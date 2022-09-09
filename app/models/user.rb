41edc5ae897771a8f486171de5452f96a53edc25
Ben Schembri <benschembri@gmail.com>
2022-09-09 14:14:53 +1000

    fixed signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,4 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :costumes, dependent: :destroy
   has_many :bookings
-
-  validates :username, presence: true, uniqueness: true
-  validates :password, presence: true
 end
